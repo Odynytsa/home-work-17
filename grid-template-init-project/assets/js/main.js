2bd53ec0a8be9d5f928c51e0141ec7b9e1cb6414
Andrey Odynytsa <odynytsaa@gmail.com>
2026-06-12 16:59:27 +0300

    Fix hamburger menu and improve responsive navbar

--- grid-template-init-project/assets/js/main.js
+++ grid-template-init-project/assets/js/main.js
@@ -2,3 +2,9 @@ $('.header__navbar-btn').on('click', function () {
   $(this).toggleClass('active')
   $('.header__navbar-list').stop(true, true).slideToggle(300)
 })
+
+// Закрывать меню при клике на ссылку
+$('.header__navbar-link').on('click', function () {
+  $('.header__navbar-btn').removeClass('active')
+  $('.header__navbar-list').slideUp(300)
+})
